967a9209536a2246ccf879b58d0fd36df4bb1816
Evans Acheampong <evansachie01@gmail.com>
2024-05-13 14:38:43 +0000

    made changes to the homepage

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCVyY_lxgBhJzcT1kA5JcDdLDpZ45HUavc",
+    authDomain: "full-stack-app-104c3.firebaseapp.com",
+    projectId: "full-stack-app-104c3",
+    storageBucket: "full-stack-app-104c3.appspot.com",
+    messagingSenderId: "726747568315",
+    appId: "1:726747568315:web:4b4975f51aaa11560d319e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
